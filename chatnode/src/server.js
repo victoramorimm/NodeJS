5fe9c010d2a61a518f0b935c8a85fa8b8e4732ad
Victor Amorim <victorvmrgamer@gmail.com>
2020-09-25 16:47:05 -0300

    Starting to use Socket.io

--- chatnode/src/server.js
+++ chatnode/src/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
-
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+let connectedUsers = [];
+
+io.on('connection', (socket) => {
+    console.log('Conexão detectada...');
+    
+     socket.on('join-request', (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+
+        socket.emit('user-ok', connectedUsers);
+        socket.broadcast.emit('list-update', {
+            joined: username,
+            list: connectedUsers,
+        });
+     })
+})
