1a362dfe82a8bbd35a1ab84b62e9478f5d1fc296
Victor Amorim <victorvmrgamer@gmail.com>
2020-10-29 22:24:46 -0300

    refactor: move EmailValidator creation to a factory helper method

--- cursopracc1/src/presentation/controllers/signup.ts
+++ cursopracc1/src/presentation/controllers/signup.ts
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/method-signature-style */
-import { HttpRequest, HttpResponse } from '../protocols/http'
+import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
-
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
