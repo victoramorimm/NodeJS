ade8095e96ba4b44f4dd5536f8ae302c1c96b5d6
Victor Amorim <victorvmrgamer@gmail.com>
2020-11-21 19:17:04 -0300

    refactor: change test name

--- cursopracc3/src/infra/criptography/bcrypt-adapter.spec.ts
+++ cursopracc3/src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct value', async () => {
+  test('Should call bcrypt with correct values', async () => {
     const salt = 12
 
     const sut = new BcryptAdapter(salt)
