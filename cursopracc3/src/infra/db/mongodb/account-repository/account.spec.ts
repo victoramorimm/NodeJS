edfb381ae284806e1ffc8b6600776ebf297fe7f3
Victor Amorim <victorvmrgamer@gmail.com>
2020-11-23 17:05:33 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ cursopracc3/src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const account = await sut.add({
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email@mail.com')
+    expect(account.password).toBe('hashed_password')
+  })
+})
