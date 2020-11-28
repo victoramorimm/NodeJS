8b790fb1f05b7a0cb2ad3f754041a1075d1a3b05
Victor Amorim <victorvmrgamer@gmail.com>
2020-11-28 16:38:33 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- cursopracc4/src/presentation/controller/signup.spec.ts
+++ cursopracc4/src/presentation/controller/signup.spec.ts
@@ -101,6 +101,8 @@ describe('SignUp Controller', () => {
   test('Should return 400 if an invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
 
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -110,11 +112,28 @@ describe('SignUp Controller', () => {
       }
     }
 
-    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    sut.handle(httpRequest)
+
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
