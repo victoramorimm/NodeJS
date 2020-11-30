056695caf0aa7adc28695956e4a2d993d267a9c7
Victor Amorim <victorvmrgamer@gmail.com>
2020-11-30 10:52:29 -0300

    refactor: group all signup protocol imports to a single file

--- cursopracc4/src/presentation/controller/signup.spec.ts
+++ cursopracc4/src/presentation/controller/signup.spec.ts
@@ -1,7 +1,5 @@
-import { AccountModel } from '../../domain/models/account'
-import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
+import { AccountModel, AddAccount, AddAccountModel, EmailValidator } from './signup-protocols'
 import { InvalidParamError, MissingParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols'
 import { SignUpController } from './signup'
 
 const makeEmailValidatorStub = (): EmailValidator => {
--- cursopracc4/src/presentation/controller/signup.ts
+++ cursopracc4/src/presentation/controller/signup.ts
@@ -1,7 +1,7 @@
-import { AddAccount } from '../../domain/usecases/add-account'
+import { AddAccount, EmailValidator } from './signup-protocols'
 import { InvalidParamError, MissingParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
+import { Controller, HttpRequest, HttpResponse } from '../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
