fe1ddfc9a6113659d404a831fea6dc78eeead6d6
Victor Amorim <victorvmrgamer@gmail.com>
2020-11-30 13:01:07 -0300

    refactor: move http 200 code to a helper

--- cursopracc4/src/presentation/controller/signup.ts
+++ cursopracc4/src/presentation/controller/signup.ts
@@ -6,7 +6,7 @@ import {
   HttpResponse
 } from './signup-protocols'
 import { InvalidParamError, MissingParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
+import { badRequest, ok, serverError } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -45,10 +45,7 @@ export class SignUpController implements Controller {
         password
       })
 
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
