372c7253fdbbb2dba1cb6448e8b05b4603b3f75c
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-01 10:31:43 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- cursopracc4/src/utils/email-validator-adapter.spec.ts
+++ cursopracc4/src/utils/email-validator-adapter.spec.ts
@@ -17,4 +17,12 @@ describe('EmailValidator Adapter', () => {
 
     expect(isValid).toBe(false)
   })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isValid = sut.isValid('valid_email@mail.com')
+
+    expect(isValid).toBe(true)
+  })
 })
