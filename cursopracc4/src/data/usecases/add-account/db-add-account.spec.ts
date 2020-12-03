51dbcb5f46836bb88facda9ba2daa82b1537461e
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-03 12:23:26 -0300

    refactor: change files path

--- cursopracc4/src/data/usecases/add-account/db-add-account.spec.ts
+++ cursopracc4/src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from '../protocols/encrypter'
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
 
 describe('DbAddAccount Usecase', () => {
