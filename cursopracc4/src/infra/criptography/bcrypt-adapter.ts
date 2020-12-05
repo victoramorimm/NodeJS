c8ef90f989675c87d322d562f1f2e703d78dc401
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-05 13:01:31 -0300

    feat: ensure BcryptAdapter returns a hash on success

--- cursopracc4/src/infra/criptography/bcrypt-adapter.ts
+++ cursopracc4/src/infra/criptography/bcrypt-adapter.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 import { Encrypter } from '../../data/protocols/encrypter'
 
-export class BcryptAdater implements Encrypter {
+export class BcryptAdapter implements Encrypter {
   private readonly salt: number
 
   constructor (salt: number) {
