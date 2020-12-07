36092b51f4c09dd9538fc5a0543219afe8b8d83c
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-07 20:01:28 -0300

    refactor: move mapping to mongo-helper

--- cursopracc4/src/infra/db/mongodb/helpers/mongo-helper.ts
+++ cursopracc4/src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -23,5 +23,11 @@ export const MongoHelper = {
     }
 
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...accountWithoutId } = collection
+
+    return Object.assign({}, accountWithoutId, { id: _id })
   }
 }
