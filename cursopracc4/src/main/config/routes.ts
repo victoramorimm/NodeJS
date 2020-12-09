4d371a25ffa9279ee729e8afe8559496c49a2710
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-08 21:30:16 -0300

    feat: ensure SignUp route returns an account on success

---
+++ cursopracc4/src/main/config/routes.ts
@@ -0,0 +1,13 @@
+import { Express, Router } from 'express'
+import fg from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+
+  app.use('/api', router)
+
+  // Automatizando o import dos routes no nosso Router.
+  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
+    (await import(`../../../${file}`)).default(router)
+  })
+}
