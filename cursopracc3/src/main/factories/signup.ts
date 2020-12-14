cc92532095ddf9349c7ad4443aca1401e333c920
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-14 13:03:01 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- cursopracc3/src/main/factories/signup.ts
+++ cursopracc3/src/main/factories/signup.ts
@@ -3,8 +3,10 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
 
   const emailValidatorAdapter = new EmailValidatorAdapter()
@@ -17,5 +19,5 @@ export const makeSignUpController = (): SignUpController => {
 
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
 
-  return signUpController
+  return new LogControllerDecorator(signUpController)
 }
--- cursopracc3/src/presentation/controllers/signup/signup.ts
+++ cursopracc3/src/presentation/controllers/signup/signup.ts
@@ -46,7 +46,6 @@ export class SignUpController implements Controller {
 
       return ok(account)
     } catch (error) {
-      console.error(error)
       return serverError()
     }
   }
