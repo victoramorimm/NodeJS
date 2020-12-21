ca5500294d87701d6290de23da8c4809969a2374
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-21 16:43:15 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- cursopracc3/src/presentation/helpers/validators/validation-composite.spec.ts
+++ cursopracc3/src/presentation/helpers/validators/validation-composite.spec.ts
@@ -53,4 +53,12 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeFalsy()
+  })
 })
