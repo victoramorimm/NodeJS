3ae6eccf8e41478fdb3274ba1fe0a9d1f46367d2
Victor Amorim <victorvmrgamer@gmail.com>
2020-12-21 17:23:38 -0300

    refactor: move HttpHelper to a folder

--- cursopracc3/src/presentation/controllers/login/login.spec.ts
+++ cursopracc3/src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from '../../errors'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 import { HttpRequest, Authentication } from './login-protocols'
 import { LoginController } from './login'
 import { Validation } from '../signup/signup-protocols'
