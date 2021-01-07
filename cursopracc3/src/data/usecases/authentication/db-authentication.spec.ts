791710ca34ecc1dfa874354e741f2c26005810ff
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-07 15:24:40 -0300

    test: ensure DbAuthentication throws if HashComparer throws

--- cursopracc3/src/data/usecases/authentication/db-authentication.spec.ts
+++ cursopracc3/src/data/usecases/authentication/db-authentication.spec.ts
@@ -107,4 +107,17 @@ describe('DbAuthentication UseCase', () => {
 
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
+
+  test('Should throw if HashComparer throws', async () => {
+    const { sut, hashComparerStub } = makeSut()
+
+    jest.spyOn(hashComparerStub, 'compare')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const fakeAuthentication = makeFakeAuthentication()
+
+    const promise = sut.auth(fakeAuthentication)
+
+    await expect(promise).rejects.toThrow()
+  })
 })
