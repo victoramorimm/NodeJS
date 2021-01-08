12767ce24141f787aec7e18cacdcc99aa8d31972
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-08 15:24:28 -0300

    refactor: move bcrypt-adapter files to a folder

--- cursopracc3/jest.config.js
+++ cursopracc3/jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '<!rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
+    '!<rootDir>/src/data/usecases/authentication/db-authentication-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/login/login-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/index.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
--- cursopracc3/src/main/factories/signup/signup.ts
+++ cursopracc3/src/main/factories/signup/signup.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../../presentation/protocols'
@@ -16,7 +16,10 @@ export const makeSignUpController = (): Controller => {
 
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const signUpController = new SignUpController(
+    dbAddAccount,
+    makeSignUpValidation()
+  )
 
   const logMongoRepository = new LogMongoRepository()
 
