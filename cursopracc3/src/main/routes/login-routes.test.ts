ee8978baa56c9f69d5c79e5ca1a4301de2ef8e5b
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-10 00:01:21 -0300

    test: ensure login route returns 401 if login fails

--- cursopracc3/src/main/routes/login-routes.test.ts
+++ cursopracc3/src/main/routes/login-routes.test.ts
@@ -52,5 +52,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 if login fails', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'victorvmrgamer@gmail.com',
+          password: '123',
+        })
+        .expect(401)
+    })
   })
 })
