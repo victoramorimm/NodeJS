a821d5f6d1b9a8eaec544aac337e55441c6e4840
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-10 00:19:00 -0300

    chore: release 1.1.0

--- cursopracc3/jest.config.js
+++ cursopracc3/jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
     '!<rootDir>/src/data/usecases/authentication/db-authentication-protocols.ts',
-    '!<rootDir>/src/presentation/controllers/login/login-protocols.ts',
-    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/login/login-controller-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/signup/signup-controller-protocols.ts',
     '!<rootDir>/src/presentation/protocols/index.ts'
   ],
   coverageDirectory: 'coverage',
