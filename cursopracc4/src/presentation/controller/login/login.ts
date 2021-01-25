a7d260e29b94cdfdf8af448a7313bcada981bdee
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-25 15:08:56 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- cursopracc4/src/presentation/controller/login/login.ts
+++ cursopracc4/src/presentation/controller/login/login.ts
@@ -7,5 +7,9 @@ export class LoginController implements Controller {
     if (!httpRequest.body.email) {
       return badRequest(new MissingParamError('email'))
     }
+
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'))
+    }
   }
 }
