e2e2a661c53df7792904d390120ab742541a12fd
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-26 18:21:39 -0300

    refactor: remove value for input on required-field-validation test

--- cursopracc4/src/presentation/helpers/validators/required-field-validation.spec.ts
+++ cursopracc4/src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -5,7 +5,7 @@ describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation returns fails', () => {
     const sut = new RequiredFieldValidation('field')
 
-    const error = sut.validate({ name: 'any_name' })
+    const error = sut.validate({})
 
     expect(error).toEqual(new MissingParamError('field'))
   })
