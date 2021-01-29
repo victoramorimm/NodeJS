a713c5ca9e1af1ef9ff78c286c09b0b0ed2ecf6e
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-29 14:24:44 -0300

    refactor: remove duplicated code

--- cursopracc4/src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ cursopracc4/src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -6,11 +6,7 @@ import {
 import { Hasher } from '../../../data/protocols/criptography/hasher'
 
 export class BcryptAdapter implements Hasher, HashComparer {
-  private readonly salt: number
-
-  constructor(salt: number) {
-    this.salt = salt
-  }
+  constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
     const hashedPassword = await bcrypt.hash(value, this.salt)
