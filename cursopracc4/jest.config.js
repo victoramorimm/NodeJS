f3084754e5969e70e3f782621a3358646be190a4
Victor Amorim <victorvmrgamer@gmail.com>
2021-01-29 16:14:31 -0300

    refactor: change controller folder name to controllers

--- cursopracc4/jest.config.js
+++ cursopracc4/jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/**/*.ts', '!<rootDir>/src/main/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
+    '!<rootDir>/src/data/usecases/authentication/db-authentication-protocols.ts',
+    '!<rootDir>/src/presentation/controller/login/login-controller-protocols.ts',
+    '!<rootDir>/src/presentation/controller/signup/singup-controller-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/index.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
--- cursopracc4/src/main/factories/login/login-factory.ts
+++ cursopracc4/src/main/factories/login/login-factory.ts
@@ -3,7 +3,7 @@ import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt
 import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
-import { LoginController } from '../../../presentation/controller/login/login-controller'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
 import { Controller } from '../../../presentation/protocols'
 import env from '../../config/env'
 
