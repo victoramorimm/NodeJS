45d9e47680c1d8cb1ae24c4f7e0b4de6af2d4352
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-04 14:47:05 -0300

    refactor: improve http-helper

--- cursopracc4/src/presentation/helpers/http/http-helper.ts
+++ cursopracc4/src/presentation/helpers/http/http-helper.ts
@@ -1,12 +1,20 @@
 import { ServerError, UnauthorizedError } from '../../errors'
 import { HttpResponse } from '../../protocols/http'
 
-export const badRequest = (error: Error): HttpResponse => {
-  return {
-    statusCode: 400,
-    body: error
-  }
-}
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
 
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401,
@@ -18,23 +26,7 @@ export const forbidden = (error: Error): HttpResponse => ({
   body: error
 })
 
-export const serverError = (error: Error): HttpResponse => {
-  return {
-    statusCode: 500,
-    body: new ServerError(error.stack)
-  }
-}
-
-export const ok = (data: any): HttpResponse => {
-  return {
-    statusCode: 200,
-    body: data
-  }
-}
-
-export const noContent = (): HttpResponse => {
-  return {
-    statusCode: 204,
-    body: null
-  }
-}
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
