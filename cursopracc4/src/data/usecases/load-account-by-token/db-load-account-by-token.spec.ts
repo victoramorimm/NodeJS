0b0e4c6e2349e96c412f77db68e617dc479b4b4a
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-06 21:08:36 -0300

    test: ensure DbLoadAccountByToken throws if Decrypter throws

--- cursopracc4/src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ cursopracc4/src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -79,6 +79,20 @@ describe('DbLoadAccount Usecase', () => {
     expect(account).toBeNull()
   })
 
+  test('Should throw if Decrypter throws', async () => {
+    const { sut, decrypterStub } = makeSut()
+
+    jest
+      .spyOn(decrypterStub, 'decrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+
+    const account = sut.load('any_token', 'any_role')
+
+    await expect(account).rejects.toThrow()
+  })
+
   test('Should call LoadAccountByTokenRepository with correct values', async () => {
     const { sut, loadAccountByTokenRepositoryStub } = makeSut()
 
