2d4cf75cfd4820eafc5c8fd661b18ef4c56db6a8
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-10 15:01:51 -0300

    test: ensure DbLoadSurveys returns a list of Surveys on success

--- cursopracc4/src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ cursopracc4/src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -67,4 +67,14 @@ describe('DbLoadSurveys Usecase', () => {
 
     expect(loadAllSpy).toHaveBeenCalled()
   })
+
+  test('Should return a list of Surveys on success', async () => {
+    const { sut } = makeSut()
+
+    const surveys = await sut.load()
+
+    const fakeSurveysList = makeFakeSurveys()
+
+    expect(surveys).toEqual(fakeSurveysList)
+  })
 })
