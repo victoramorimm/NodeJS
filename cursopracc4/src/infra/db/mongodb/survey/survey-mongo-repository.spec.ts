b68a2fb4695f5796369d456d7ca645b1a3ac425c
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-11 14:00:54 -0300

    test: ensure SurveyMongoRepository loads all surveys on success

--- cursopracc4/src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ cursopracc4/src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -53,4 +53,45 @@ describe('Survey Mongo Repository', () => {
       expect(survey).toBeTruthy()
     })
   })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              image: 'other_image',
+              answer: 'other_answer'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ],
+          date: new Date()
+        }
+      ])
+
+      const sut = makeSut()
+
+      const surveysFromDb = await sut.loadAll()
+
+      expect(surveysFromDb.length).toBe(2)
+      expect(surveysFromDb[0].question).toBe('any_question')
+      expect(surveysFromDb[1].question).toBe('other_question')
+    })
+  })
 })
