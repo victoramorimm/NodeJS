f8ab2f1fcb70dfb135b1727ba19d558755080c7d
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-11 14:00:42 -0300

    feat: ensure SurveyMongoRepository loads all surveys on success

---
+++ cursopracc4/src/data/protocols/survey/survey-model.ts
@@ -0,0 +1,8 @@
+import { SurveyAnswerModel } from '../../../domain/models/survey'
+
+export interface SurveyModel {
+  id: string
+  question: string
+  answers: SurveyAnswerModel[]
+  date: Date
+}
