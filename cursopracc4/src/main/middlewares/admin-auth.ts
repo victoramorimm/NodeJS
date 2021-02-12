45275918e9dacfd9d1b469210ecf54e462255809
Victor Amorim <victorvmrgamer@gmail.com>
2021-02-11 21:23:22 -0300

    fix: add obligated role to admin-auth

--- cursopracc4/src/main/middlewares/admin-auth.ts
+++ cursopracc4/src/main/middlewares/admin-auth.ts
@@ -1,4 +1,4 @@
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
-export const adminAuth = adaptMiddleware(makeAuthMiddleware())
+export const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
